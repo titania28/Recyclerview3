9bdeb35504550a55259ce2fd2d15518b642da077
Titania Putri <titaniaputri28@gmail.com>
2016-11-05 20:24:36 +0700

    RecyclerView List Card

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -0,0 +1,17 @@
+package id.sch.smktelkom_mlg.learn.recyclerview3.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Titania Putri on 05/11/2016.
+ */
+public class Hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+    public Hotel(String judul, String deskripsi, Drawable foto){
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
